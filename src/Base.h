c15eacfcfb602c54bb1c6937935712f4da5930af
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-02 22:44:42 +0200

    sort includes in Base.h

--- src/Base.h
+++ src/Base.h
@@ -3,26 +3,26 @@
 // this header should be included on all header files
 // it is also going to be precompiled in _precompile.h
 
-#include <string>
-#include <map>
-#include <vector>
 #include <cassert>
-#include <memory>
-#include <stdexcept>
-#include <type_traits>
 #include <fstream>
-#include <sstream>
 #include <iostream>
-#include <utility>
 #include <iterator>
+#include <map>
+#include <memory>
 #include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 #include "Python.h"
+#include <boost/iterator/iterator_facade.hpp>
+#include <boost/iterator/iterator_facade.hpp>
 #include <boost/python.hpp>
-#include <boost/iterator/iterator_facade.hpp>
-namespace py = boost::python;
 
-#include <boost/iterator/iterator_facade.hpp>
+namespace py = boost::python;
 
 #include "v8.h"
 
