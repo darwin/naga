eedaf3bcb3a30f16b5556b319185529a25ef30ee
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-06 22:10:02 +0200

    review JSStackFrame

--- src/JSStackFrame.cpp
+++ src/JSStackFrame.cpp
@@ -27,3 +27,36 @@ std::string CJSStackFrame::GetFunctionName() const {
 
   return std::string(*name, name.length());
 }
+
+CJSStackFrame::CJSStackFrame(v8::Isolate* v8_isolate, v8::Local<v8::StackFrame> v8_stack_frame)
+    : m_v8_isolate(v8_isolate), m_v8_frame(v8_isolate, v8_stack_frame) {}
+
+CJSStackFrame::CJSStackFrame(const CJSStackFrame& stack_frame) : m_v8_isolate(stack_frame.m_v8_isolate) {
+  v8::HandleScope handle_scope(m_v8_isolate);
+
+  m_v8_frame.Reset(m_v8_isolate, stack_frame.Handle());
+}
+
+v8::Local<v8::StackFrame> CJSStackFrame::Handle() const {
+  return v8::Local<v8::StackFrame>::New(m_v8_isolate, m_v8_frame);
+}
+
+int CJSStackFrame::GetLineNumber() const {
+  v8::HandleScope handle_scope(m_v8_isolate);
+  return Handle()->GetLineNumber();
+}
+
+int CJSStackFrame::GetColumn() const {
+  v8::HandleScope handle_scope(m_v8_isolate);
+  return Handle()->GetColumn();
+}
+
+bool CJSStackFrame::IsEval() const {
+  v8::HandleScope handle_scope(m_v8_isolate);
+  return Handle()->IsEval();
+}
+
+bool CJSStackFrame::IsConstructor() const {
+  v8::HandleScope handle_scope(m_v8_isolate);
+  return Handle()->IsConstructor();
+}
--- src/JSStackFrame.h
+++ src/JSStackFrame.h
@@ -7,37 +7,17 @@ class CJSStackFrame {
   v8::Persistent<v8::StackFrame> m_v8_frame;
 
  public:
-  CJSStackFrame(v8::Isolate* isolate, v8::Local<v8::StackFrame> frame)
-      : m_v8_isolate(isolate), m_v8_frame(isolate, frame) {}
+  CJSStackFrame(v8::Isolate* v8_isolate, v8::Local<v8::StackFrame> v8_stack_frame);
+  CJSStackFrame(const CJSStackFrame& stack_frame);
 
-  CJSStackFrame(const CJSStackFrame& frame) : m_v8_isolate(frame.m_v8_isolate) {
-    v8::HandleScope handle_scope(m_v8_isolate);
+  [[nodiscard]] v8::Local<v8::StackFrame> Handle() const;
 
-    m_v8_frame.Reset(m_v8_isolate, frame.Handle());
-  }
-
-  [[nodiscard]] v8::Local<v8::StackFrame> Handle() const {
-    return v8::Local<v8::StackFrame>::New(m_v8_isolate, m_v8_frame);
-  }
-
-  [[nodiscard]] int GetLineNumber() const {
-    v8::HandleScope handle_scope(m_v8_isolate);
-    return Handle()->GetLineNumber();
-  }
-  [[nodiscard]] int GetColumn() const {
-    v8::HandleScope handle_scope(m_v8_isolate);
-    return Handle()->GetColumn();
-  }
+  [[nodiscard]] int GetLineNumber() const;
+  [[nodiscard]] int GetColumn() const;
   [[nodiscard]] std::string GetScriptName() const;
   [[nodiscard]] std::string GetFunctionName() const;
-  [[nodiscard]] bool IsEval() const {
-    v8::HandleScope handle_scope(m_v8_isolate);
-    return Handle()->IsEval();
-  }
-  [[nodiscard]] bool IsConstructor() const {
-    v8::HandleScope handle_scope(m_v8_isolate);
-    return Handle()->IsConstructor();
-  }
+  [[nodiscard]] bool IsEval() const;
+  [[nodiscard]] bool IsConstructor() const;
 
   static void Expose(const pb::module& m);
 };
