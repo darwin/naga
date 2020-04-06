fef1e23532da56c65f9b50af34ab16d5062e0dd1
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-06 13:24:47 +0200

    scripts: pass all remaining arguments to python -m unittest

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,6 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
-TEST_GLOB=${1:-*}
-
 cd "$ROOT_DIR"
 
 ./scripts/create-venv.sh
@@ -14,4 +12,6 @@ set -x
 cd tests
 
 export PATH=$VENV_DIR/bin:/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-python -m unittest discover -p "$TEST_GLOB"
+python -m unittest discover --failfast --locals "$@"
+#python -m trace --listfuncs test_Multithread.py
+#python test_Multithread.py
