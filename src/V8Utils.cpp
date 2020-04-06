f949049b3837a92b344007461ef5f4ff993c49da
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-06 23:02:22 +0200

    cleanup V8Utils

--- src/V8Utils.cpp
+++ src/V8Utils.cpp
@@ -4,23 +4,6 @@
 
 namespace v8u {
 
-//std::optional<v8::Local<v8::String>> toStringDirectly(py::object obj) {
-//  if (PyUnicode_CheckExact(obj.ptr())) {
-//    auto py_bytes = PyUnicode_AsUTF8String(obj.ptr());  // may be NULL
-//    py::object bytes_obj(py::handle<>(py::allow_null(py_bytes)));
-//    return pythonBytesObjectToString(bytes_obj.ptr());
-//  }
-//
-//  if (PyBytes_CheckExact(obj.ptr())) {
-//    return pythonBytesObjectToString(obj.ptr());
-//  }
-//
-//  // please note that returning nullopt is different than
-//  // returning v8::Local<v8::String> with empty value inside
-//  // and that is different than returning v8::Local<v8::String> with empty string "" inside
-//  return std::nullopt;
-//}
-
 std::optional<v8::Local<v8::String>> toStringDirectly(pb::handle obj) {
   if (PyUnicode_CheckExact(obj.ptr())) {
     auto py_bytes = PyUnicode_AsUTF8String(obj.ptr());  // may be NULL
@@ -38,36 +21,17 @@ std::optional<v8::Local<v8::String>> toStringDirectly(pb::handle obj) {
   return std::nullopt;
 }
 
-//v8::Local<v8::String> toString(py::object str) {
-//  // first try to convert python string object directly if possible
-//  auto v8_str = toStringDirectly(str);
-//  if (v8_str) {
-//    return *v8_str;
-//  }
-//
-//  // alternatively convert it to string representation and try again
-//  auto py_str = PyObject_Str(str.ptr());  // may be NULL
-//  auto str_obj = py::object(py::handle<>(py::allow_null(py_str)));
-//  v8_str = toStringDirectly(str_obj);
-//  if (v8_str) {
-//    return *v8_str;
-//  }
-//
-//  // all attempts failed, return empty value
-//  return v8::Local<v8::String>();
-//}
-
-v8::Local<v8::String> toString(pb::handle str) {
+v8::Local<v8::String> toString(pb::handle py_str) {
   // first try to convert python string object directly if possible
-  auto v8_str = toStringDirectly(str);
+  auto v8_str = toStringDirectly(py_str);
   if (v8_str) {
     return *v8_str;
   }
 
   // alternatively convert it to string representation and try again
-  auto py_str = PyObject_Str(str.ptr());  // may be NULL
-  auto str_obj = pb::reinterpret_steal<pb::object>(py_str);
-  v8_str = toStringDirectly(str_obj);
+  auto py_computed_str = PyObject_Str(py_str.ptr());  // may be NULL
+  auto py_computed_obj = pb::reinterpret_steal<pb::object>(py_computed_str);
+  v8_str = toStringDirectly(py_computed_obj);
   if (v8_str) {
     return *v8_str;
   }
