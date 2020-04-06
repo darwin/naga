0d4e1e51ae2f8d737c01ef373670dee9e56f17be
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-06 23:08:06 +0200

    stop using debug macros

--- src/Debug.h
+++ src/Debug.h
@@ -2,11 +2,11 @@
 
 #include "Base.h"
 
-#undef Py_INCREF
-#define Py_INCREF(op) debugPyIncRef(op)
-
-#undef Py_DECREF
-#define Py_DECREF(op) debugPyDecRef(op)
+//#undef Py_INCREF
+//#define Py_INCREF(op) debugPyIncRef(op)
+//
+//#undef Py_DECREF
+//#define Py_DECREF(op) debugPyDecRef(op)
 
 void debugPyIncRef(PyObject* op);
 void debugPyDecRef(PyObject* op);
