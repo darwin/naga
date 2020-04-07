fbdc6bd429fb9d8fc2d762f889f2b106f7673bc9
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-07 14:11:59 +0200

    teach build.sh to deal with --verbose flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,23 +4,60 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
+# poor man's bash flags parsing
+# https://stackoverflow.com/a/14203146/84283
+POSITIONAL_OPTS=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+  -v | --verbose)
+    STPYV8_VERBOSE=1
+    shift
+    ;;
+  *) # unknown option
+    POSITIONAL_OPTS+=("$1")
+    shift
+    ;;
+  esac
+done
+set -- "${POSITIONAL_OPTS[@]}" # restore positional parameters
+
 BUILD_CONFIG=${1:-release}
 
-cd "$ROOT_DIR"
+EXTRA_GEN_BUILD_ARGS=()
+if [[ -n "$STPYV8_VERBOSE" ]]; then
+  EXTRA_GEN_BUILD_ARGS+=("--verbose")
+fi
 
-echo_cmd ./scripts/gen-build.sh "$BUILD_CONFIG"
-
-OUT_DIR="_out/$STPYV8_V8_GIT_TAG/$BUILD_CONFIG"
-
-echo_cmd ./scripts/enter_depot_shell.sh ninja -C "$OUT_DIR" stpyv8
-
-echo_cmd cd "$EXT_DIR"
+EXTRA_NINJA_ARGS=()
+if [[ -n "$STPYV8_VERBOSE" ]]; then
+  EXTRA_NINJA_ARGS+=("-v")
+fi
 
 EXTRA_BUILD_ARGS=()
 if [[ "$BUILD_CONFIG" == "debug" ]]; then
   EXTRA_BUILD_ARGS+=("--debug")
 fi
+if [[ -n "$STPYV8_VERBOSE" ]]; then
+  EXTRA_BUILD_ARGS+=("--verbose")
+fi
+
+EXTRA_INSTALL_ARGS=()
+if [[ -n "$STPYV8_VERBOSE" ]]; then
+  EXTRA_INSTALL_ARGS+=("--verbose")
+fi
+
+OUT_DIR="_out/$STPYV8_V8_GIT_TAG/$BUILD_CONFIG"
+
+cd "$ROOT_DIR"
+
+echo_cmd ./scripts/gen-build.sh "${EXTRA_GEN_BUILD_ARGS[@]}" "$BUILD_CONFIG"
+
+echo_cmd ./scripts/enter_depot_shell.sh ninja "${EXTRA_NINJA_ARGS[@]}" -C "$OUT_DIR" stpyv8
+
+echo_cmd cd "$EXT_DIR"
 
 echo_cmd python3 setup.py build "${EXTRA_BUILD_ARGS[@]}"
 
-echo_cmd python3 setup.py install --prefix "$VENV_DIR"
+echo_cmd python3 setup.py install "${EXTRA_INSTALL_ARGS[@]}" --prefix "$VENV_DIR"
--- scripts/gen-build.sh
+++ scripts/gen-build.sh
@@ -4,6 +4,30 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
+# poor man's bash flags parsing
+# https://stackoverflow.com/a/14203146/84283
+POSITIONAL_OPTS=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+  -v | --verbose)
+    STPYV8_VERBOSE=1
+    shift
+    ;;
+  *) # unknown option
+    POSITIONAL_OPTS+=("$1")
+    shift
+    ;;
+  esac
+done
+set -- "${POSITIONAL_OPTS[@]}" # restore positional parameters
+
+STPYV8_GN_ARGS=()
+if [[ -n "$STPYV8_VERBOSE" ]]; then
+  STPYV8_GN_ARGS+=( "stpyv8_verbose_build=true" )
+fi
+
 cd "$GN_DIR"
 
 # see https://v8.dev/docs/source-code
@@ -28,7 +52,8 @@ done
 export PATH=$DEPOT_HOME:$VENV2_DIR/bin:/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 echo_cmd gn gen --verbose "$BUILD_DIR" \
-  --args="import(\"//args/${BUILD_CONFIG_NAME}.gn\") $STPYV8_GN_EXTRA_ARGS" $STPYV8_GN_GEN_EXTRA_ARGS
+  --args="import(\"//args/${BUILD_CONFIG_NAME}.gn\") $STPYV8_GN_EXTRA_ARGS ${STPYV8_GN_ARGS[@]}" \
+  $STPYV8_GN_GEN_EXTRA_ARGS
 
 if [[ -z "$STPYV8_GN_GEN_EXTRA_ARGS" ]]; then
   echo_info "in depot shell, you may now use following commands:"
