67dfdeb7e600b6604a495c8679636b4e1a278fe3
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-11 19:39:17 +0200

    some minor refactoring in Tracer

--- src/Tracer.cpp
+++ src/Tracer.cpp
@@ -1,14 +1,14 @@
 #include "Tracer.h"
 
 ObjectTracer::ObjectTracer(v8::Local<v8::Value> v8_handle, PyObject* raw_object)
-    : m_v8_handle(v8u::getCurrentIsolate(), v8_handle), m_raw_object(raw_object), m_living(GetLivingMapping()) {
+    : m_v8_handle(v8u::getCurrentIsolate(), v8_handle), m_raw_object(raw_object), m_living_ptr(GetLivingMap()) {
   Py_INCREF(m_raw_object);
 }
 
 ObjectTracer::~ObjectTracer() {
   if (!m_v8_handle.IsEmpty()) {
     Dispose();
-    m_living->erase(m_raw_object);
+    m_living_ptr->erase(m_raw_object);
   }
 }
 
@@ -26,10 +26,10 @@ ObjectTracer& ObjectTracer::Trace(v8::Local<v8::Value> v8_handle, PyObject* raw_
 }
 
 void ObjectTracer::Trace() {
-  m_living->insert(std::make_pair(m_raw_object, this));
+  m_living_ptr->insert(std::make_pair(m_raw_object, this));
 }
 
-LivingMap2* ObjectTracer::GetLivingMapping() {
+LivingMap* ObjectTracer::GetLivingMap() {
   auto v8_isolate = v8u::getCurrentIsolate();
   auto v8_scope = v8u::openScope(v8_isolate);
   auto v8_context = v8_isolate->GetCurrentContext();
@@ -42,7 +42,7 @@ LivingMap2* ObjectTracer::GetLivingMapping() {
   if (!v8_value.IsEmpty()) {
     auto v8_val = v8_value.ToLocalChecked();
     if (v8_val->IsExternal()) {
-      LivingMap2* living = static_cast<LivingMap2*>(v8_val.As<v8::External>()->Value());
+      LivingMap* living = static_cast<LivingMap*>(v8_val.As<v8::External>()->Value());
 
       if (living) {
         return living;
@@ -50,7 +50,7 @@ LivingMap2* ObjectTracer::GetLivingMapping() {
     }
   }
 
-  std::unique_ptr<LivingMap2> living(new LivingMap2());
+  std::unique_ptr<LivingMap> living(new LivingMap());
 
   auto v8_living = v8::External::New(v8u::getCurrentIsolate(), living.get());
   v8_context->Global()->SetPrivate(v8_context, v8_key_api, v8_living);
@@ -61,7 +61,7 @@ LivingMap2* ObjectTracer::GetLivingMapping() {
 }
 
 v8::Local<v8::Value> ObjectTracer::FindCache(PyObject* raw_object) {
-  auto living = GetLivingMapping();
+  auto living = GetLivingMap();
   if (living) {
     auto it = living->find(raw_object);
     if (it != living->end()) {
@@ -75,7 +75,7 @@ py::object ObjectTracer::Object() const {
   return py::reinterpret_borrow<py::object>(m_raw_object);
 }
 
-ContextTracer::ContextTracer(v8::Local<v8::Context> v8_context, LivingMap2* living)
+ContextTracer::ContextTracer(v8::Local<v8::Context> v8_context, LivingMap* living)
     : m_v8_context(v8u::getCurrentIsolate(), v8_context), m_living(living) {}
 
 ContextTracer::~ContextTracer() {
@@ -98,7 +98,7 @@ void ContextTracer::WeakCallback(const v8::WeakCallbackInfo<ContextTracer>& v8_i
   delete v8_info.GetParameter();
 }
 
-void ContextTracer::Trace(v8::Local<v8::Context> v8_context, LivingMap2* living) {
+void ContextTracer::Trace(v8::Local<v8::Context> v8_context, LivingMap* living) {
   auto tracer = new ContextTracer(v8_context, living);
   tracer->Trace();
 }
--- src/Tracer.h
+++ src/Tracer.h
@@ -4,22 +4,22 @@
 
 class ObjectTracer;
 
-typedef std::map<PyObject*, ObjectTracer*> LivingMap2;
+typedef std::map<PyObject*, ObjectTracer*> LivingMap;
 
 class ObjectTracer {
   v8::Persistent<v8::Value> m_v8_handle;
   PyObject* m_raw_object;
-  LivingMap2* m_living;
+  LivingMap* m_living_ptr;
 
   void Trace();
 
-  static LivingMap2* GetLivingMapping();
+  static LivingMap* GetLivingMap();
 
  public:
   ObjectTracer(v8::Local<v8::Value> v8_handle, PyObject* raw_object);
   ~ObjectTracer();
 
-  py::object Object() const;
+  [[nodiscard]] py::object Object() const;
 
   void Dispose();
   static ObjectTracer& Trace(v8::Local<v8::Value> v8_handle, PyObject* raw_object);
@@ -28,16 +28,16 @@ class ObjectTracer {
 
 class ContextTracer {
   v8::Persistent<v8::Context> m_v8_context;
-  std::unique_ptr<LivingMap2> m_living;
+  std::unique_ptr<LivingMap> m_living;
 
   void Trace();
   static void WeakCallback(const v8::WeakCallbackInfo<ContextTracer>& v8_info);
 
  public:
-  ContextTracer(v8::Local<v8::Context> v8_context, LivingMap2* living);
+  ContextTracer(v8::Local<v8::Context> v8_context, LivingMap* living);
   ~ContextTracer();
 
-  v8::Local<v8::Context> Context() const;
+  [[nodiscard]] v8::Local<v8::Context> Context() const;
 
-  static void Trace(v8::Local<v8::Context> v8_context, LivingMap2* living);
+  static void Trace(v8::Local<v8::Context> v8_context, LivingMap* living);
 };
