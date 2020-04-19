9f76364c6707cc43ed38521aadedf9c5c66e226e
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-20 00:25:08 +0200

    rename CallThis to ForwardToThis

--- src/JSToolkitExpose.cpp
+++ src/JSToolkitExpose.cpp
@@ -4,24 +4,24 @@
   LOGGER_INDENT;   \
   SPDLOG_LOGGER_TRACE(getLogger(kJSObjectLogger), __VA_ARGS__)
 
-// CallThis is a helper class which redirects function calls to instance calls (expects first arg to be the instance)
+// ForwardToThis is a helper class which redirects function calls to instance calls (expects first arg to be the instance)
 //
-// in python: JSObject.something(instance, arg1, arg2, ...)
+// in python: toolkit.something(instance, arg1, arg2, ...)
 // calls (via pybind) our wrapper's operator(), and we in turn call
 // instance->Something(arg1, arg2, ...)
 // credit: https://stackoverflow.com/a/46533854/84283
 template <auto CJSObjectAPI::*F>
-struct CallThis {};
+struct ForwardToThis {};
 
 // this is a specialization for const member functions
 template <class RET, class... ARGS, auto (CJSObjectAPI::*F)(ARGS...) const->RET>
-struct CallThis<F> {
+struct ForwardToThis<F> {
   auto operator()(const CJSObjectPtr& obj, ARGS&&... args) const { return ((*obj).*F)(std::forward<ARGS>(args)...); }
 };
 
 // this is a specialization for non-const member functions
 template <class RET, class... ARGS, auto (CJSObjectAPI::*F)(ARGS...)->RET>
-struct CallThis<F> {
+struct ForwardToThis<F> {
   auto operator()(const CJSObjectPtr& obj, ARGS&&... args) const { return ((*obj).*F)(std::forward<ARGS>(args)...); }
 };
 
@@ -30,49 +30,51 @@ void exposeJSToolkit(py::module py_module) {
   py::module m = py_module.def_submodule("toolkit", "Javascript Toolkit");
 
   // clang-format off
-  m.def("linenum", CallThis<&CJSObjectAPI::PythonLineNumber>{},
+  m.def("linenum", ForwardToThis<&CJSObjectAPI::PythonLineNumber>{},
         py::arg("this"),
         "The line number of function in the script");
-  m.def("colnum", CallThis<&CJSObjectAPI::PythonColumnNumber>{},
+  m.def("colnum", ForwardToThis<&CJSObjectAPI::PythonColumnNumber>{},
         py::arg("this"),
         "The column number of function in the script");
-  m.def("resname", CallThis<&CJSObjectAPI::PythonResourceName>{},
+  m.def("resname", ForwardToThis<&CJSObjectAPI::PythonResourceName>{},
         py::arg("this"),
         "The resource name of script");
-  m.def("inferredname", CallThis<&CJSObjectAPI::PythonInferredName>{},
+  m.def("inferredname", ForwardToThis<&CJSObjectAPI::PythonInferredName>{},
         py::arg("this"),
         "Name inferred from variable or property assignment of this function");
-  m.def("lineoff", CallThis<&CJSObjectAPI::PythonLineOffset>{},
+  m.def("lineoff", ForwardToThis<&CJSObjectAPI::PythonLineOffset>{},
         py::arg("this"),
         "The line offset of function in the script");
-  m.def("coloff", CallThis<&CJSObjectAPI::PythonColumnOffset>{},
+  m.def("coloff", ForwardToThis<&CJSObjectAPI::PythonColumnOffset>{},
         py::arg("this"),
         "The column offset of function in the script");
-  m.def("set_name", CallThis<&CJSObjectAPI::PythonSetName>{},
+  m.def("set_name", ForwardToThis<&CJSObjectAPI::PythonSetName>{},
         py::arg("this"),
         py::arg("name"));
-  m.def("get_name", CallThis<&CJSObjectAPI::PythonGetName>{},
+  m.def("get_name", ForwardToThis<&CJSObjectAPI::PythonGetName>{},
         py::arg("this"));
 
-  m.def("apply", CallThis<&CJSObjectAPI::PythonApply>{},
+  m.def("apply", ForwardToThis<&CJSObjectAPI::PythonApply>{},
         py::arg("this"),
         py::arg("self"),
         py::arg("args") = py::list(),
         py::arg("kwds") = py::dict(),
         "Performs a function call using the parameters.");
-  m.def("invoke", CallThis<&CJSObjectAPI::PythonInvoke>{},
+  m.def("invoke", ForwardToThis<&CJSObjectAPI::PythonInvoke>{},
         py::arg("this"),
         py::arg("args") = py::list(),
         py::arg("kwds") = py::dict(),
         "Performs a binding method call using the parameters.");
-  m.def("clone", CallThis<&CJSObjectAPI::PythonClone>{},
+  m.def("clone", ForwardToThis<&CJSObjectAPI::PythonClone>{},
         py::arg("this"),
         "Clone the object.");
+
   m.def("create", &CJSObjectAPI::PythonCreateWithArgs,
         py::arg("constructor"),
         py::arg("arguments") = py::tuple(),
         py::arg("propertiesObject") = py::dict(),
         "Creates a new object with the specified prototype object and properties.");
+
   m.def("hasJSArrayRole", [](const CJSObjectPtr &obj) { return obj->HasRole(CJSObjectBase::Roles::JSArray); });
   m.def("hasJSFunctionRole", [](const CJSObjectPtr &obj) { return obj->HasRole(CJSObjectBase::Roles::JSFunction); });
   m.def("hasCLJSObjectRole", [](const CJSObjectPtr &obj) { return obj->HasRole(CJSObjectBase::Roles::CLJSObject); });
