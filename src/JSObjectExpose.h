442dd6b7e1f84c9325d061e4618f658ab9175fd0
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-20 00:18:24 +0200

    remove some unused includes

--- src/JSObjectExpose.h
+++ src/JSObjectExpose.h
@@ -1,10 +1,5 @@
 #pragma once
 
 #include "Base.h"
-#include "JSObjectBase.h"
-#include "JSObjectGenericImpl.h"
-#include "JSObjectFunctionImpl.h"
-#include "JSObjectArrayImpl.h"
-#include "JSObjectCLJSImpl.h"
 
 void exposeJSObject(py::module py_module);
--- src/JSToolkitExpose.h
+++ src/JSToolkitExpose.h
@@ -1,10 +1,5 @@
 #pragma once
 
 #include "Base.h"
-#include "JSObjectBase.h"
-#include "JSObjectGenericImpl.h"
-#include "JSObjectFunctionImpl.h"
-#include "JSObjectArrayImpl.h"
-#include "JSObjectCLJSImpl.h"
 
 void exposeJSToolkit(py::module py_module);
