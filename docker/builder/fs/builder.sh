081d480d98c1ef842b476d8382d114382f39f1ad
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-28 21:37:05 +0200

    docker: add test-examples sub command

--- docker/builder/fs/builder.sh
+++ docker/builder/fs/builder.sh
@@ -5,6 +5,8 @@ set -e -o pipefail
 export NAGA_WORK_DIR="/naga-work"
 export NAGA_LDFLAGS="-L/usr/lib -L/usr/lib/x86_64-linux-gnu"
 export NAGA_INCLUDES="-I/usr/include -I/usr/include/x86_64-linux-gnu"
+export NAGA_EXAMPLES_EXPECTATIONS_DIR=
+export NAGA_EXAMPLES_RESULTS_DIR=
 
 cd "/naga"
 
@@ -17,6 +19,9 @@ if [[ "$1" == "build" ]]; then
 elif [[ "$1" == "test" ]]; then
   shift
   ./scripts/test.sh "$@"
+elif [[ "$1" == "test-examples" ]]; then
+  shift
+  ./scripts/test-examples.sh "$@"
 elif [[ "$1" == "enter" ]]; then
   shift
   "$@"
