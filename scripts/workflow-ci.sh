d95217420f50f795ea398466aab04834fab7fe31
Antonin Hildebrand <antonin@hildebrand.cz>
2020-04-28 15:34:46 +0200

    workflow: another failed approach to docker caching

--- scripts/workflow-ci.sh
+++ scripts/workflow-ci.sh
@@ -5,70 +5,48 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
-NAGA_WORKFLOW_CACHE_DIR=${NAGA_WORKFLOW_CACHE_DIR:-"$NAGA_WORK_DIR/workflow-cache"}
-NAGA_CACHE_BACKUP_FILE_PATH="$NAGA_WORKFLOW_CACHE_DIR/$NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME-backup.tar.bz2"
-NAGA_CACHE_BACKUP_FILE_DIR="$(dirname "$NAGA_CACHE_BACKUP_FILE_PATH")"
+if [[ -n "$GITHUB_WORKFLOW" ]]; then
+  # https://medium.com/developer-space/how-to-change-docker-data-folder-configuration-33d372669056
+  # shellcheck disable=SC2016
+  SERVICE_CONFIG="/lib/systemd/system/docker.service"
+  set -x
+  sudo ps aux | grep -i docker
+  sudo systemctl stop docker
+  sudo ps aux | grep -i docker
+  CACHED_DOCKER_HOME=~/docker
+  sudo sed -i "s#-H fd://#-H fd:// -g $CACHED_DOCKER_HOME#g" "$SERVICE_CONFIG"
 
-NAGA_IMAGE_BACKUP_PATH="$NAGA_WORKFLOW_CACHE_DIR/$NAGA_DOCKER_BUILDER_IMAGE_NAME-backup.save"
-NAGA_IMAGE_BACKUP_DIR="$(dirname "$NAGA_IMAGE_BACKUP_PATH")"
-
-load_docker_volumes() {
-  if [[ -f "$NAGA_CACHE_BACKUP_FILE_PATH" ]]; then
-    echo "Loading docker volume $NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME from '$NAGA_CACHE_BACKUP_FILE_PATH'"
-    docker volume rm -f "$NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME"
-    docker volume create --name "$NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME" >/dev/null
-    docker run --rm -i \
-      -v "$NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME:/target" \
-      busybox tar -xjC /target <"$NAGA_CACHE_BACKUP_FILE_PATH"
-  else
-    echo "Skipping loading docker volume $NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME"
-    echo "because '$NAGA_CACHE_BACKUP_FILE_PATH' does not exist"
-  fi
+  mkdir -p "$CACHED_DOCKER_HOME"
+  cat <<EOF | sudo tee "/etc/docker/daemon.json"
+{
+ "data-root": "$CACHED_DOCKER_HOME"
 }
+EOF
+  cat /etc/docker/daemon.json || :
+  cat "$SERVICE_CONFIG" || :
 
-save_docker_volumes() {
-  echo "Exporting docker volume $NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME to '$NAGA_CACHE_BACKUP_FILE_PATH'"
-  if [[ ! -d "$NAGA_CACHE_BACKUP_FILE_DIR" ]]; then
-    mkdir -p "$NAGA_CACHE_BACKUP_FILE_DIR"
-  fi
-  docker system prune -f || :
-  docker run --rm \
-    -v "$NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME:/source:ro" \
-    busybox tar -cjC /source . >"$NAGA_CACHE_BACKUP_FILE_PATH"
-}
+#  chmod -R root:root "$CACHED_DOCKER_HOME"
 
-load_docker_images() {
-  if [[ -f "$NAGA_IMAGE_BACKUP_PATH" ]]; then
-    echo "Loading docker image file: $NAGA_IMAGE_BACKUP_PATH"
-    docker load -q -i "$NAGA_IMAGE_BACKUP_PATH"
-  else
-    echo "Skipping loading docker image $NAGA_DOCKER_BUILDER_IMAGE_NAME"
-    echo "because '$NAGA_IMAGE_BACKUP_PATH' does not exist"
-  fi
-}
-
-save_docker_images() {
-  echo "Saving docker image file: $NAGA_IMAGE_BACKUP_PATH"
-  if [[ ! -d "$NAGA_IMAGE_BACKUP_DIR" ]]; then
-    mkdir -p "$NAGA_IMAGE_BACKUP_DIR"
-  fi
-  docker save "$NAGA_DOCKER_BUILDER_IMAGE_NAME:latest" -o "$NAGA_IMAGE_BACKUP_PATH"
-}
+  sudo systemctl daemon-reload
+  sudo systemctl start docker
+  sudo ps aux | grep -i docker
+fi
 
 set -x
 cd "$ROOT_DIR/docker/builder"
 
-# this caching is not that important, rebuilding the docker image is pretty fast
-load_docker_images
 ./do.sh build
-save_docker_images
 
-# our docker image uses a separate volume for its caches (NAGA_DOCKER_BUILDER_CACHE_VOLUME_NAME)
-# we try to persist that between github workflow runs by exporting and importing the content as suggested by
-# https://stackoverflow.com/a/53622621/84283
-# the NAGA_WORKFLOW_CACHE_DIR should be cached by github
-load_docker_volumes
 ./do.sh run build
-save_docker_volumes
+
+if [[ -n "$GITHUB_WORKFLOW" ]]; then
+  # hack tar to always run as root
+  # this is needed for cache task to properly serialize docker files
+  set -x
+  ls -la /bin/tar
+  sudo chown root /bin/tar
+  sudo chmod a+s /bin/tar
+  ls -la /bin/tar
+fi
 
 ./do.sh run test
