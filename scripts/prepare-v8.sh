b993344e37edd931a86b146494ab48e4af58d089
Antonin Hildebrand <antonin@hildebrand.cz>
2020-05-06 01:05:41 +0200

    scripts: properly update existing v8 checkout in V8_HOME

--- scripts/prepare-v8.sh
+++ scripts/prepare-v8.sh
@@ -8,23 +8,20 @@ cd "$ROOT_DIR"
 
 # see https://v8.dev/docs/source-code
 
-# we should fetch either when $V8_HOME does not exist or when it is empty
-# the ls is here to fix corner case when gclient creates the directory
-# but fails to fetch it and leaves it emtpy
-if [[ ! -d "$V8_HOME" || -z "$(ls -A "$V8_HOME")" ]]; then
+if [[ ! -d "$V8_HOME" ]]; then
   V8_HOME_PARENT="$V8_HOME/.."
   if [[ ! -d "$V8_HOME_PARENT" ]]; then
     echo_cmd mkdir -p "$V8_HOME_PARENT"
   fi
-  cd "$V8_HOME_PARENT"
-  # we have to force fetch to prevent error:
-  # "Your current directory appears to already contain, or be part of a checkout."
-  # it complains because we are doing a checkout under our git repo (in vendor/v8)
-  echo_cmd fetch --force v8
 fi
 
+cd "$V8_HOME_PARENT"
+# we have to force fetch to prevent error:
+# "Your current directory appears to already contain, or be part of a checkout."
+# it complains because we are doing a checkout under our git repo (in vendor/v8)
+echo_cmd fetch --force v8 # note that this will also update our repo if it exists and will fetch missing tags
+
 echo_cmd cd "$V8_HOME"
-echo_cmd git fetch --tags
 echo_cmd git checkout "$NAGA_V8_GIT_TAG"
 
 activate_python3
