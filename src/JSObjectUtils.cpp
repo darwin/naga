7bc264b24a7e950fe0a6c6d06b7306744d4b8b3f
Antonin Hildebrand <antonin@hildebrand.cz>
2020-05-06 00:16:32 +0200

    make isCLJSType more robust against empty handles

--- src/JSObjectUtils.cpp
+++ src/JSObjectUtils.cpp
@@ -17,13 +17,18 @@ bool isCLJSType(v8::Local<v8::Object> v8_obj) {
 
   // TODO: optimize this
   auto v8_ctor_key = v8::String::NewFromUtf8(v8_isolate, "constructor").ToLocalChecked();
-  auto v8_ctor_val = v8_obj->Get(v8_context, v8_ctor_key).ToLocalChecked();
+  auto v8_ctor = v8_obj->Get(v8_context, v8_ctor_key);
 
-  if (v8_ctor_val.IsEmpty() || !v8_ctor_val->IsObject()) {
+  if (v8_ctor.IsEmpty()) {
     return false;
   }
 
-  auto v8_ctor_obj = v8::Local<v8::Object>::Cast(v8_ctor_val);
+  auto v8_ctor_val = v8_ctor.ToLocalChecked();
+  if (!v8_ctor_val->IsObject()) {
+    return false;
+  }
+
+  auto v8_ctor_obj = v8_ctor_val.As<v8::Object>();
   auto v8_cljs_key = v8::String::NewFromUtf8(v8_isolate, "cljs$lang$type").ToLocalChecked();
   auto v8_cljs_val = v8_ctor_obj->Get(v8_context, v8_cljs_key).ToLocalChecked();
 
